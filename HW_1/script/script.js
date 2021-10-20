267bffeca2ebd13c26fb88c26b2e139bc75dd48d
Sasha <krivella6@mail.ru>
2021-10-20 20:22:45 +0400

    fist_hw_js

---
+++ HW_1/script/script.js
@@ -0,0 +1,21 @@
+"use strict"
+let q = 6;
+let w = 2;
+let e = q*w;
+
+console.log(e)
+
+
+let n = 45;
+let t = n%10;  // выдает остаток от деления на 10 ==> 5
+let m = (n-t)/10;  // считаем целое число без остатка
+let o = t+m;
+
+console.log(o)
+
+
+let a = 7;
+let b = 3;
+a=a-4
+b=b+4
+console.log(a,b)
